f2452dc60265b5307b3eebd811082d80aad10f41
aman-118 <66792236+aman-118@users.noreply.github.com>
2021-04-21 19:55:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 from flask import Flask,request,redirect,render_template,url_for
+
+app=Flask(__Name__)
+
+@app.route('/')
+def home():
+    cources = Cource.query.all()
+    return render_template('index.html',courc=cources)
+
+  
+  if __Name__=="__main__":
+    app.run()
